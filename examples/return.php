8f139c99140e1ecc2232c22cb0115eb86cd5fed5
Brent Shepherd <thenbrent@gmail.com>
2011-07-06 19:50:36 +1000

    Only showing confirm form when user has not already started the subscirption.

--- examples/return.php
+++ examples/return.php
@@ -1,18 +1,13 @@
 <?php
 require_once('functions.php');
 $response = $paypal->get_checkout_details();
-$subscription = $paypal->get_subscription_details( 'class-properties' );
+$subscription = $paypal->get_subscription_details();
 ?>
 <html>
 <head>
 	<title>Confirm your payment</title>
 </head>
 <body>
-	<h2>Confirm Your Subscription</h2>
-	<p>Total: <?php echo $subscription->amount . ' per ' . $subscription->period; ?></p>
-	<form action='' method='post'>
-		<input type='submit' name='confirm' value='Confirm' />
-	</form>
 	<?php 
 	if( isset( $_POST['confirm'] ) && $_POST['confirm'] == "Confirm" ) {
 		$doresponse = $paypal->start_subscription();
@@ -24,6 +19,13 @@ $subscription = $paypal->get_subscription_details( 'class-properties' );
 		} else if($doresponse['ACK'] == "Failure" || $doresponse['ACK'] == "FailureWithWarning") {
 			echo "<p>The API Call Failed: " . urldecode($doresponse['L_LONGMESSAGE0']) . "</p>";
 		}
+	} else { ?>
+		<h2>Confirm Your Subscription</h2>
+		<p>Total: <?php echo $subscription->amount . ' per ' . $subscription->period; ?></p>
+		<form action='' method='post'>
+			<input type='submit' name='confirm' value='Confirm' />
+		</form>
+	<?php 
 	}
 	?>
 </body>
