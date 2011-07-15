0d8541509d2e61339acc529e20b7195c897f0e8e
Brent Shepherd <thenbrent@gmail.com>
2011-07-15 13:14:42 +1000

    Using get_script_uri() function.

--- examples/functions.php
+++ examples/functions.php
@@ -6,8 +6,9 @@ global $paypal;
 
 require_once('../paypal-digital-goods.class.php');
 
+// Create our PayPal Object
 $credentials = array(
-	'username'  => 'your_api_username',  // For quick and dirty tests, use 'digita_1308916325_biz_api1.gmail.com',
+	'username'  => 'your_api_username',  // For quick and once off sandbox tests, use 'digita_1308916325_biz_api1.gmail.com',
 	'password'  => 'your_api_password',  // '1308916362',
 	'signature' => 'your_api_signature', // 'AFnwAcqRkyW0yPYgkjqTkIGqPbSfAyVFbnFAjXCRltVZFzlJyi2.HbxW'
 );
@@ -15,10 +16,23 @@ $credentials = array(
 if( $credentials['username'] == 'your_api_username' || $credentials['password'] == 'your_api_password' || $credentials['signature'] == 'your_api_signature' )
 	exit( 'You must set your API credentials in /examples/functions.php for this example to work.' );
 
-$urls = array(
-	'return_url' => 'http://localhost/paypal-digital-goods/examples/return.php?return=paid',
-	'cancel_url' => 'http://localhost/paypal-digital-goods/examples/return.php?return=cancel',
+$args = array(
+	'return_url' => get_script_uri( 'return.php' ) . '?paypal=paid',
+	'cancel_url' => get_script_uri( 'return.php' ) . '?paypal=cancel',
 );
 
-$paypal = new PayPal_Digital_Goods( $credentials, $urls );
+$paypal = new PayPal_Digital_Goods( $credentials, $args );
 
+
+function get_script_uri( $script = 'index.php' ){
+	// IIS Fix
+	if( empty( $_SERVER['REQUEST_URI'] ) )
+		$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
+
+	// Strip off query string
+	$url = preg_replace( '/\?.*$/', '', $_SERVER['REQUEST_URI'] );
+	//$url = 'http://'.$_SERVER['HTTP_HOST'].'/'.ltrim(dirname($url), '/').'/';
+	$url = 'http://'.$_SERVER['HTTP_HOST'].implode( '/', ( explode( '/', $_SERVER['REQUEST_URI'], -1 ) ) ) . '/';
+
+	return $url . $script;
+}
