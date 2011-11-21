6980ff6050871774f95356466b435e94293f0165
Brent Shepherd <thenbrent@gmail.com>
2011-11-21 14:13:40 +1000

    Updating example index to include a simple purchase example.

--- examples/index.php
+++ examples/index.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Example: Simple Payment
+ * Examples
  * 
- * This example shows the simplest method of accepting a payment.
+ * A few examples to demonstrate the methods of accepting payments.
  */
 
 ?>
@@ -14,16 +14,19 @@
 <body>
 	<div class="container">
 		<h1>Thanks for Dropping by!</h1>
-		<p>This library includes two examples: <a href="example-simple.php">Simple</a> and <a href="example-checkout.php">Separate Checkout</a>.</p>
+		<p>This library includes three examples: <a href="example-purchase.php">Simple Purchase</a>, <a href="example-subscription.php">Simple Subscription</a> and <a href="example-subscription-checkout.php">Subscription with Separate Checkout</a>.</p>
 
-		<h3>Simple Example</h3>
-		<p>As the name suggests, this is the example demonstrates the simplest method for using the library to accept Digital Goods Subscriptions.</p>
-		<p>Although simple to integrate, the payment page is slow to load as it requests a checkout token on page load.</p>
-		<p><a href="example-simple.php">Try the Simple Example &raquo;</a></p>
+		<h3>Simple Purchase</h3>
+		<p>This example demonstrates how to use the library to accept a Digital Goods payment.</p>
+		<p><a href="example-purchase.php">Try the Simple Purchase Example &raquo;</a></p>
 
-		<h3>Separate Checkout</h3>
-		<p>This example shows how to use a separate checkout page in the payment flow. Doing so greatly reduces the page load of your payment page by requesting a checkout token from PayPal only after a visitor has committed to payment.</p>
-		<p><a href="example-checkout.php">Try the Separate Checkout Example &raquo;</a></p>
+		<h3>Simple Subscription</h3>
+		<p>Create a Digital Goods Subscription by following the this example.</p>
+		<p><a href="example-subscription.php">Try the Simple Subscription Example &raquo;</a></p>
+
+		<h3>Subscription with Separate Checkout</h3>
+		<p>This example shows how to use a separate checkout page in the payment flow. Doing so greatly reduces the page load of your payment/sign-up page by requesting a checkout token from PayPal only after a visitor has committed to payment.</p>
+		<p><a href="example-subscription-checkout.php">Try the Separate Checkout Example &raquo;</a></p>
 	</div>
 </body>
 </html>
