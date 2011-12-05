becedbc9c21a488c73cf6aafb1c6ffa95cc57924
Brent Shepherd <thenbrent@gmail.com>
2011-12-05 11:53:07 +1000

    Using PayPal_Digital_Goods_Configuration class in example via set_credentials() function.

--- examples/functions.php
+++ examples/functions.php
@@ -12,23 +12,27 @@ require_once( '../paypal-purchase.class.php' );
 /**
  * Used to create a central function for getting credentials for both Subscription & Purchase objects
  */
-function get_credentials() {
-	// Create our PayPal Object
-	$credentials = array(
+function set_credentials() {
+
 	/*
-		'username'  => 'your_api_username',  // For quick and once off sandbox tests, use 'digita_1308916325_biz_api1.gmail.com',
-		'password'  => 'your_api_password',  // '1308916362',
-		'signature' => 'your_api_signature', // 'AFnwAcqRkyW0yPYgkjqTkIGqPbSfAyVFbnFAjXCRltVZFzlJyi2.HbxW'
+	PayPal_Digital_Goods_Configuration::username( 'your_api_username' );
+	PayPal_Digital_Goods_Configuration::password( 'your_api_password' );
+	PayPal_Digital_Goods_Configuration::signature( 'your_api_signature' );
 	*/
-		'username'  => 'digita_1308916325_biz_api1.gmail.com',
-		'password'  => '1308916362',
-		'signature' => 'AFnwAcqRkyW0yPYgkjqTkIGqPbSfAyVFbnFAjXCRltVZFzlJyi2.HbxW'
-	);
 
-	if( $credentials['username'] == 'your_api_username' || $credentials['password'] == 'your_api_password' || $credentials['signature'] == 'your_api_signature' )
+	PayPal_Digital_Goods_Configuration::username( 'digita_1308916325_biz_api1.gmail.com' );
+	PayPal_Digital_Goods_Configuration::password( '1308916362' );
+	PayPal_Digital_Goods_Configuration::signature( 'AFnwAcqRkyW0yPYgkjqTkIGqPbSfAyVFbnFAjXCRltVZFzlJyi2' );
+
+	PayPal_Digital_Goods_Configuration::return_url( get_script_uri( 'return.php?paypal=paid' ) );
+	PayPal_Digital_Goods_Configuration::cancel_url( get_script_uri( 'return.php?paypal=cancel' ) );
+	PayPal_Digital_Goods_Configuration::business_name( get_script_uri( 'Demo Store' ) );
+
+	'business_name' => 'Demo Store',
+
+
+	if( PayPal_Digital_Goods_Configuration::username() == 'your_api_username' || PayPal_Digital_Goods_Configuration::password() == 'your_api_password' || PayPal_Digital_Goods_Configuration::signature() == 'your_api_signature' )
 		exit( 'You must set your API credentials in ' . __FILE__ . ' for this example to work.' );
-
-	return $credentials;
 }
 
 /**
@@ -36,20 +40,15 @@ function get_credentials() {
  */
 function create_example_purchase() {
 
-	$args = array(
-		'return_url'    => get_script_uri( 'return.php?paypal=paid' ),
-		'cancel_url'    => get_script_uri( 'return.php?paypal=cancel' ),
-		'business_name' => 'Demo Store',
-		'purchase'  => array(
-			'item_name'   => 'Digital Good Example',
-			'description' => 'Example Digital Good Purchase',
-			// Price
-			'amount'      => '12.00',
-		)
+	set_credentials();
+
+	$purchase_details = array(
+		'item_name'   => 'Digital Good Example',
+		'description' => 'Example Digital Good Purchase',
+		'amount'      => '12.00',
 	);
 
-	//return new PayPal_Digital_Goods( get_credentials(), $args );
-	return new PayPal_Purchase( $args['purchase'] );
+	return new PayPal_Purchase( $purchase_details );
 }
 
 
@@ -59,20 +58,17 @@ function create_example_purchase() {
  */
 function create_example_subscription() {
 
-	$args = array(
-		'return_url'    => get_script_uri( 'return.php?paypal=paid' ),
-		'cancel_url'    => get_script_uri( 'return.php?paypal=cancel' ),
-		'business_name' => 'Demo Store',
-		'subscription'  => array(
-			'initial_amount'     => '10.00',
-			'amount'             => '2.00',
-			'period'             => 'Week',
-			'frequency'          => '1', 
-			'total_cycles'       => '4',
-		)
+	set_credentials();
+
+	$subscription_details = array(
+		'initial_amount'     => '10.00',
+		'amount'             => '2.00',
+		'period'             => 'Week',
+		'frequency'          => '1', 
+		'total_cycles'       => '4',
 	);
 
-	return new PayPal_Digital_Goods( get_credentials(), $args );
+	return new PayPal_Subscription( $subscription_details );
 }
 
 
